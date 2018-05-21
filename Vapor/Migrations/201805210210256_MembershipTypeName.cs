efaf64e111eb957923d65cf22276235f250acf29
Arvin Itaas <itaasarvin@gmail.com>
2018-05-20 22:25:05 -0400

    Implemented MembershipTypeNames

---
+++ Vapor/Migrations/201805210210256_MembershipTypeName.cs
@@ -0,0 +1,21 @@
+namespace Vapor.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MembershipTypeName : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("UPDATE MembershipType SET Name='Pay As You Go' WHERE Id=1");
+            Sql("UPDATE MembershipType SET Name='Monthly' WHERE Id=2");
+            Sql("UPDATE MembershipType SET Name='Quarterly' WHERE Id=3");
+            Sql("UPDATE MembershipType SET Name='Annual' WHERE Id=4");
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.MembershipTypes", "Name");
+        }
+    }
+}
