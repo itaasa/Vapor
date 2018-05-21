9dcf00a9c3f130befa711c2bb7cc5eebfc75bd59
Arvin Itaas <itaasarvin@gmail.com>
2018-05-20 20:42:36 -0400

    Initial commit

---
+++ Vapor/Controllers/CustomersController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vapor.Models;
+
+namespace Vapor.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+        [Route("Customers/Details/{id}")]
+        public ActionResult Details(int id)
+        {
+            var customers = GetCustomers();
+            foreach (var customer in customers)
+            {
+                if (customer.Id == id)
+                    return View(customer);
+            }
+
+            return HttpNotFound();
+        }
+
+        private List<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+
+                new Customer
+                {
+                    Id=1, Name = "John Smith"
+                },
+                new Customer
+                {
+                    Id=2, Name = "Mary Williams"
+                }
+            };
+        }
+    }
+}
---
+++ Vapor/Controllers/GamesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vapor.Models;
+
+namespace Vapor.Controllers
+{
+    public class GamesController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            var games = GetGames();
+            return View(games);
+
+        }
+
+        [Route("Games/Details/{id}")]
+        public ActionResult Details(int id)
+        {
+            var games = GetGames();
+            foreach (var game in games)
+            {
+                if (game.Id == id)
+                    return View(game);
+            }
+
+            return HttpNotFound();
+        }
+
+        private List<Game> GetGames()
+        {
+            return new List<Game>
+            {
+                new Game
+                {
+                    Id = 1,
+                    Name = "Escape From Tarkov"
+                },
+                new Game
+                {
+                    Id = 2,
+                    Name = "Eve Online"
+                },
+                new Game
+                {
+                    Id = 3,
+                    Name = "Dota 2"
+                }
+            };
+        }
+    }
+}
